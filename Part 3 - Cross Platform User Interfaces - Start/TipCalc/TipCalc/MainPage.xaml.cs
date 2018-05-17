a4d288fe4dd3b452abf282d10f1691cf724d90f6
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-05-17 00:43:10 -0400

    Added the start/final project for part 3

---
+++ Part 3 - Cross Platform User Interfaces - Start/TipCalc/TipCalc/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TipCalc
+{
+	public partial class MainPage : ContentPage
+	{
+        public TipCalculator tipCalculator;
+
+		public MainPage()
+		{
+			InitializeComponent();           
+
+            tipCalculator = new TipCalculator
+            {
+                Total = 100,
+                TipPercent = 18
+            };
+            tipCalculator.CalcTip();
+
+            BindingContext = tipCalculator;
+        }
+	}
+}
