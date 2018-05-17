109056374d32db60cb771b49ef4a33f75d736308
Richard L. Taylor Jr <rtaylor@sentryone.com>
2018-05-16 23:31:21 -0400

    Added Start/Final solutions for Part 2

---
+++ Part 2 - Sharing Business Logic - Final/TipCalc/TipCalc/TipCalculator.cs
@@ -0,0 +1,47 @@
+﻿namespace TipCalc
+{
+    public class TipCalculator
+    {
+        public decimal Total { get; set; }
+
+        public decimal Tip { get; set; }
+
+        public decimal TipPercent { get; set; }
+
+        public decimal GrandTotal { get; private set; }
+
+        public TipCalculator() { }
+
+        public void CalcTip()
+        {
+            if (TipPercent > 0)
+            {
+                Tip = Total * (TipPercent / 100);
+            }
+            else
+            {
+                Tip = 0;
+            }
+            UpdateGrandTotal();
+        }
+
+        public void CalcTipPercentage()
+        {
+            if (Total > 0)
+            {
+                TipPercent = (Tip / Total) * 100;
+            }
+            else
+            {
+                TipPercent = 0;
+            }
+
+            UpdateGrandTotal();
+        }
+
+        private void UpdateGrandTotal()
+        {
+            GrandTotal = Total + Tip;
+        }
+    }
+}
